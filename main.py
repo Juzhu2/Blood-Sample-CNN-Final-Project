9bd5ccef7f5ad77b68708bf386c8527db1a76108
Juzhu2 <justinzhu1224@gmail.com>
2025-03-01 12:54:01 -0800

    fixed looped

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 
 
 list_o_transformaiton = v2.Compose([
-    v2.ToTensor()
+    v2.ToTensor(),
 ])
 
 testSimpleImgs = torchImg.ImageFolder(root="dataset2-master/images/TEST_SIMPLE", transform=list_o_transformaiton)
@@ -19,6 +19,13 @@ testSimpleData = DataLoader(testSimpleImgs, batch_size=32, shuffle=True)
 trainingData = DataLoader(TrainImgs, batch_size=32, shuffle=True)
 testData = DataLoader(TestImgs, batch_size=32, shuffle=True)
 
-for image, label in enumerate(trainingData):
-    print(f'Image: {image}')
-    print(f'Label: {label}')
+for idx,(image, label) in enumerate(testSimpleData):
+    print(label)
+    
+# for idx,(image, label) in enumerate(trainingData):
+#     print(f'Image: {image}')
+#     print(f'Label: {label}')
+
+# for image, label in testData:
+#     print(f'Image: {image}')
+#     print(f'Label: {label}')
