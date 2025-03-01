dd183aa46a9a9c8f894e07bf51287f7a36acc334
Juzhu2 <justinzhu1224@gmail.com>
2025-03-01 12:16:21 -0800

    added loop for dataloader

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np 
+from matplotlib import pyplot as plt
+import torchvision.datasets as torchImg
+from torchvision.transforms import v2
+from torch.utils.data import DataLoader
+
+
+
+list_o_transformaiton = v2.Compose([
+    v2.ToTensor()
+])
+
+testSimpleImgs = torchImg.ImageFolder(root="dataset2-master/images/TEST_SIMPLE", transform=list_o_transformaiton)
+TrainImgs = torchImg.ImageFolder(root="dataset2-master/images/TRAIN", transform=list_o_transformaiton)
+TestImgs = torchImg.ImageFolder(root="dataset2-master/images/TEST", transform=list_o_transformaiton)
+
+
+testSimpleData = DataLoader(testSimpleImgs, batch_size=32, shuffle=True)
+trainingData = DataLoader(TrainImgs, batch_size=32, shuffle=True)
+testData = DataLoader(TestImgs, batch_size=32, shuffle=True)
+
+for image, label in enumerate(trainingData):
+    print(f'Image: {image}')
+    print(f'Label: {label}')
