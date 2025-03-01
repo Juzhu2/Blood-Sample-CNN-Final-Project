43dbf5c4d0b71c93eea949bcc9488619a00fc5d0
Alexis Valdes <avaldesg@ucsc.edu>
2025-03-01 14:07:13 -0800

    Added Transformations

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from torch.utils.data import DataLoader
 
 list_o_transformaiton = v2.Compose([
     v2.ToTensor(),
+    v2.GaussianBlur(kernel_size=3, sigma=5), 
+    v2.RandomGrayscale(p=0.3),
+    v2.ColorJitter(brightness=(0.5, 1.5), contrast=1.0, saturation=1.0, hue=1.0)
 ])
 
 testSimpleImgs = torchImg.ImageFolder(root="dataset2-master/images/TEST_SIMPLE", transform=list_o_transformaiton)
@@ -20,12 +23,16 @@ trainingData = DataLoader(TrainImgs, batch_size=32, shuffle=True)
 testData = DataLoader(TestImgs, batch_size=32, shuffle=True)
 
 for idx,(image, label) in enumerate(testSimpleData):
-    print(label)
+    print(f"idx: {idx}")
+    print(f'Image: {image.shape}')
+    print(f'Label: {label.shape}')
     
-# for idx,(image, label) in enumerate(trainingData):
-#     print(f'Image: {image}')
-#     print(f'Label: {label}')
+for idx,(image, label) in enumerate(trainingData):
+    print(f"idx: {idx}")
+    print(f'Image: {image.shape}')
+    print(f'Label: {label.shape}')
 
-# for image, label in testData:
-#     print(f'Image: {image}')
-#     print(f'Label: {label}')
+for image, label in testData:
+    print(f"idx: {idx}")
+    print(f'Image: {image.shape}')
+    print(f'Label: {label.shape}')
